e548fb73475eb0c893f589bd3e5d0f41c1a9fdc2
homeworkdome <homeworkdome@outlook.com>
2023-05-07 15:51:36 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingNWC/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+           
+                ViewPlan viewPlan = new FilteredElementCollector(doc)
+                                        .OfClass(typeof(ViewPlan))
+                                        .Cast<ViewPlan>()
+                                        .FirstOrDefault(v => v.ViewType == ViewType.FloorPlan &&
+                                                           v.Name.Equals("Level 1"));
+
+                var nwcOption = new NavisworksExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc",  nwcOption);
+            
+
+            return Result.Succeeded;
+        }
+    }
+}
